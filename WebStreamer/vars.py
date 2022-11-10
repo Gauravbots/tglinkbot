41eb3b889c4bb4c65ee4cd3cbc64417c58444c59
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-11-10 20:54:54 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -14,10 +14,10 @@ class Var(object):
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(getenv("BIN_CHANNEL", "-1001713974897"))
-    PORT = int(getenv('PORT', 8080))
+    PORT = int(getenv('PORT', 8000))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     OWNER_ID = int(getenv('OWNER_ID', '2062513342'))
-    NO_PORT = bool(getenv('NO_PORT', False))
+    NO_PORT = bool(getenv('NO_PORT', True))
     APP_NAME = None
     if 'DYNO' in environ:
         ON_HEROKU = True
